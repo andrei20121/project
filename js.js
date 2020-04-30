898341f0e8c3f440c4a57fc03be8e09db48f9b68
Andrei <example@gmail.ru>
2020-04-30 15:48:00 +0300

    lesson 2

---
+++ js.js
@@ -0,0 +1,38 @@
+/*let num = 51;
+
+if(num > 50) {
+    console.log('bad')
+} 
+else if(num < 5) {
+    console.log('ok')
+}
+else if(num == 50) {
+    console.log('great')
+}
+
+(num == 51) ? console.log('ok') : console.log('bad');
+
+switch (num) {
+    case num > 50:
+        console.log('dina vredina');
+        break;
+    case num < 30:
+        console.log('dina zasiha');
+        break;
+    case 50:
+        console.log('dina molodet');
+        break;
+    default:
+        console.log('andrei umnichka');
+        break;
+}
+
+let num = 10;
+while (num < 15) {
+    console.log(num);
+    num++;
+}
+
+for ( let i = 1; i < 8; i++) {
+    console.log(i)
+}*/
